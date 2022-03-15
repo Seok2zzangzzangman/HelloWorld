966e5904dec3e298ef7e41c1831f55406f3c9b36
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-03-15 15:19:18 +0900

    복

---
+++ src/Helloworld.java
@@ -0,0 +1,11 @@
+
+public class Helloworld {
+	public static void main(String[]args) {
+		System.out.println("Hello world");
+		System.out.println("Hello Soyong");
+		System.out.println("Hello Seok");
+		System.out.println("Hello Every one");
+		
+	}
+
+}
